8f680c791acc75e6337663809323cfcc59d1794e
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-02-01 21:40:14 -0500

    Added generator logic.

--- index.js
+++ index.js
@@ -1,43 +1,108 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
+
+    // Project Title
     {
         type: "input",
         name: "title",
-        message: "What is the name of your project?",
+        message: "What is the name of your project?:",
     },
+
+    // Project Description
     {
         type: "input",
         name: "description",
-        message: "Please give a brief description of this application",
+        message: "Please give a brief description of this application:",
     },
+
+    // Installation Description 
     {
         type: "input",
         name: "installation",
-        message: "Please provide the steps and software requirements to install your application",
+        message: "Please provide the steps and software requirements to install your application:",
     },
+
+    // Usage Details
     {
         type: "input",
         name: "usage",
-        message: "Please describe how this application should be used",
+        message: "Please describe how this application should be used:",
     },
+
+    // List of Licenses
     {
         type: "list",
         name: "license",
-        message: "Please select the license type"
+        message: "Please select the license type:",
         choices: 
+        [
+            "MIT License",
+            "Apache License 2.0",
+            "GNU General Public License (GPL) Version 3",
+            "BSD-3-Clause",
+            "Cryptographic Autonomy License",
+            "Mozilla Public License 2.0",
+            "None",
+        ]
+    },
+
+    // Contributors on Project
+    {
+        type: "input",
+        name: "contributors",
+        message: "Please include any contributors on this project:",
+    },
+
+    // Tests that were run, if any
+    {
+        type: "input",
+        name: "tests",
+        message: "Please explain testing implementation, if any:"
+    },
+
+    // GitHub Username
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your github username:"
+    },
+
+    // Email Address
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address:"
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, answers) {
+    const data = generateMarkdown(answers)
+    fs.writeFile("README.md", data, (err) => {
+        if (err) {
+            console.log("Error Writing to File", err);
+        }
+        else {
+            console.log("Data has been written to", fileName);
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        writeToFile("README.md", answers);
+    })
+    .catch(error => {
+        console.log("Prompt Error", error);
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,49 +1,132 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = " ";
+
+  switch (license) {
+    // Switch statement for difference licenses
+    case "MIT License":
+      badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+
+    case "Apache License 2.0":
+      badge = "[![License: Apache License 2.0](https://img.shields.io/badge/License-Apache-blue.svg)](https://opensource.org/license/apache-2-0)";
+      break;
+
+    case "GNU General Public License (GPL) Version 3":
+      badge = "[![License: GNU General Public License Version 3](https://img.shields.io/badge/License-GPL-purple.svg)](https://opensource.org/license/gpl-3-0)";
+      break;
+
+    case "BSD-3-Clause":
+      badge = "[![License: BSD-3-Clause](https://img.shields.io/badge/License-BSD3-yellow.svg)](https://opensource.org/license/bsd-3-clause/)";
+      break;
+
+    case "Cryptographic Autonomy License":
+      badge = "[![License: Cryptographic Autonomy License](https://img.shields.io/badge/License-CALv1.0-green.svg)](https://opensource.org/license/cal-1-0/)";
+      break;
+
+    case "Mozilla Public License 2.0":
+      badge = "[![License: Mozilla Public License 2.0](https://img.shields.io/badge/License-MPLv2-green.svg)](https://opensource.org/license/mpl-2-0/)";
+      break;
+
+    default:
+      badge = " ";
+      break;
+  }
+
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link = " ";
+
+  // Switch statement for difference license
+  switch (license) {
+    case "MIT License":
+      link = "https://opensource.org/licenses/MIT";
+      break;
+
+    case "Apache License 2.0":
+      link = "https://opensource.org/license/apache-2-0";
+      break;
+
+    case "GNU General Public License (GPL) Version 3":
+      link = "https://opensource.org/license/gpl-3-0/";
+      break;
+
+    case "BSD-3-Clause":
+      link = "https://opensource.org/license/bsd-3-clause";
+      break;
+
+    case "Cryptographic Autonomy License":
+      link = "https://opensource.org/license/cal-1-0/";
+      break;
+
+    case "Mozilla Public License 2.0":
+      link = "https://opensource.org/license/mpl-2-0/";
+      break;
+
+    default:
+      link = " ";
+      break;
+  }
+
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
+  [${license}](${renderLicenseLink(license)}) for more information.
+    `
+  }
+  else {
+    return " ";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data){
-  return `# ${data.title}
+function generateMarkdown(answers) {
+
+  const licenseSection = renderLicenseSection(answers.license)
+
+  return `# ${answers.title} 
+
+  ${renderLicenseBadge(answers.license)}
 
   ## Table of Contents
-  - [Description]{#description}
-  - [Installation]{#installation}
-  - [Usage]{#usage}
-  - [License]{#license}
-  - [Contributors]{#contributors}
-  - [Tests]{#tests}
-  - [Questions]{#questions} 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Tests](#tests)
+  - [Questions](#questions) 
 
   ## Description
-  ${data.description}
+  ${answers.description}
 
   ## Installation
-  ${data.installation}
+  ${answers.installation}
 
   ## Usage
-  ${data.usage}
+  ${answers.usage}
 
-  ## Licence
-  ${data.license}
+  ${licenseSection}
 
   ## Contributors
-  ${data.contributors}
+  ${answers.contributors}
 
   ## Tests
-  ${data.tests}
+  ${answers.tests}
 
   ## Questions
-  For more details please see GitHub repository ${data.github}. 
-  To reach out to me directly, please contact me at ${data.email}.
+  For more details please see GitHub repository https://github.com/${answers.github}. 
+  To reach out to me directly, please contact me at ${answers.email}.
  `;
 }
 
